e56fa1a6990c6e07a5a5733ab867fe74239be546
Steel <79706171+Steel-skull@users.noreply.github.com>
2024-03-25 10:06:04 -0500

    Update logger.py
    reformat

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,24 +1,12 @@
 import logging
 
 def setup_custom_logger(name):
-    """
-    Creates a custom logger with the specified name and configuration.
-    
-    Args:
-        name (str): The name of the logger to create. Typically, this is the name of the module.
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    Returns:
-        logging.Logger: The configured logger.
-    """
-    
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
-    
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
-    
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
-    
     return logger
